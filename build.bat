4a28abf6d1b3b7d1d02bf9b73ff3a7fcd9427e28
Martin Balogh <martin.balogh1985@gmail.com>
2025-10-24 10:32:12 +0300

    Build command for python messenger_client added to build file

--- build.bat
+++ build.bat
@@ -1,2 +1,5 @@
 gcc Messenger_server/*.c -o server.exe -Wall -Wextra -pedantic -g
 gcc Messenger_client/*.c -o messenger.exe -Wall -Wextra -pedantic -g
+
+:: Build Python client
+pyinstaller --onefile --noconsole --name messenger_python --distpath . ./Python_client/main.py
